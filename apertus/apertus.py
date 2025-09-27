642b115770fcb83f6d2cf84d9eff8eb547b9ce3a
thomascorcoral <tms.corcoral@gmail.com>
2025-09-27 13:36:17 +0200

    update apertus

--- apertus/apertus.py
+++ apertus/apertus.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 from langchain_openai import ChatOpenAI
-
+import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 APERTUS_MODEL_NAME = "swiss-ai/Apertus-70B"
@@ -19,26 +19,33 @@ class OpenaiApertus(OpenAI):
         )
 
 
-class LangchainApertus(ChatOpenAI):
-    """Langchain based integration"""
-    def __init__(self, api_key: str, **kwargs):
-        super().__init__(
-            openai_api_key=api_key,
-            model=APERTUS_MODEL_NAME,
-            base_url=APERTUS_BASE_URL,
-            **kwargs,
-        )
+GEMINI_LLM = os.getenv("GEMINI_LLM", "FALSE") == "TRUE"
+if GEMINI_LLM:
+    print("STARTING WITH GEMINI")
+    class LangchainApertus(ChatGoogleGenerativeAI):
+        def __init__(self, api_key: str, **kwargs):
+            super().__init__(
+                model="gemini-2.5-flash-lite",
+                temperature=0,
+                max_tokens=None,
+                timeout=None,
+                max_retries=2,
+                # other params...
+            )
+else:
+    print("STARTING WITH APERTUS")
+    class LangchainApertus(ChatOpenAI):
+        """Langchain based integration"""
+        def __init__(self, api_key: str, **kwargs):
+            
+            super().__init__(
+                openai_api_key=api_key,
+                model=APERTUS_MODEL_NAME,
+                base_url=APERTUS_BASE_URL,
+                **kwargs,
+            )
 
-class LLangchainApertus(ChatGoogleGenerativeAI):
-    def __init__(self, api_key: str, **kwargs):
-        super().__init__(
-            model="gemini-2.5-flash-lite",
-            temperature=0,
-            max_tokens=None,
-            timeout=None,
-            max_retries=2,
-            # other params...
-        )
+
 
 
 if __name__ == "__main__":
